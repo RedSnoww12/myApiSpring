d067c0664164bdfcbae8beb8344b8ea5ce8b33c4
amara_s <amara_s@etna-alternance.net>
2023-09-25 20:21:52 +0200

    add: docker compose

--- src/test/java/com/etna/myapi/MyapiApplicationTests.java
+++ src/test/java/com/etna/myapi/MyapiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.etna.myapi;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyapiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
